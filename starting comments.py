288879991b9055426c8f0dd2dae26a881e2e508f
Mathew Kemp <111265496+KempOra@users.noreply.github.com>
2023-06-26 11:17:12 +1000

    starting comments for projects

---
+++ starting comments.py
@@ -0,0 +1,8 @@
+#
+# File: welcome.py
+# Author: your name
+# Email Id: your email id
+# Description: Practical 1 - displays a welcome message to the screen.
+# This is my own work as defined by the University's
+# Academic Misconduct policy.
+#
