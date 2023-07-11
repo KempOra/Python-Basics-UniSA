f7da319e2c4c754ec4007b6ff9f6a1b1c35af04d
Mathew Kemp <111265496+KempOra@users.noreply.github.com>
2023-07-11 13:30:06 +1000

    -

---
+++ Favourites.py
@@ -0,0 +1,20 @@
+#
+# File: fave.py
+# Author: mathew kemp
+# Email Id: kemmy088
+# Description: asks for inputs and returns them
+# This is my own work as defined by the University's
+# Academic Misconduct policy.
+#
+
+print("whats your favourite colour?")
+colour = input()
+print(colour + " is a lovely colour!")
+
+print("whats your favourite football team?")
+team = input()
+print(team + " are definitely in my top 5")
+
+print("whats your favourite drink?")
+drink = input()
+print(drink + " is good, but better not have too many!")
