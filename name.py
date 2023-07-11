ea4fe588fc781ccf77ab0ac28f888a03b907073d
Mathew Kemp <111265496+KempOra@users.noreply.github.com>
2023-07-11 12:48:02 +1000

    followed problem set

--- name.py
+++ name.py
@@ -6,3 +6,9 @@
 # This is my own work as defined by the University's
 # Academic Misconduct policy.
 #
+
+print("please enter your name:")
+name = input()
+print("+--------+")
+print("| " + name + " |")
+print("+--------+")
