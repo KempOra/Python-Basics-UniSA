3299c81e4aa2f0b38276a3a9faf082b1580d9aa0
Mathew Kemp <111265496+KempOra@users.noreply.github.com>
2023-07-11 13:35:20 +1000

    -

---
+++ sums.py
@@ -0,0 +1,19 @@
+#
+# File: sums.py
+# Author: Mathew Kemp
+# Email Id: Kemmy088
+# Description: asks for numbers, returns total
+# This is my own work as defined by the University's
+# Academic Misconduct policy.
+#
+
+print('Please enter number 1: ')
+num1 = int(input())
+print('Please enter number 2: ')
+num2 = int(input())
+print('Please enter number 3: ')
+num3 = int(input())
+
+total = num1 + num2 + num3
+
+print("The sum is: " , total)
